e22aa4d36ae60383d6540ac6498caa43edd169e3
Julien Terraz <julien@terraz.org>
2018-03-04 10:55:07 +0100

    MOVING FROM DROPBOX

--- README.md
+++ README.md
@@ -12,6 +12,9 @@ Install python 3.5
 
 sudo apt-get install python3-pip
 sudo pip3 install watchdog
+sudo pip3 install dropbox
+
+
 # You may have to wait a long time before it start instaling, be patient.
 
 
@@ -30,8 +33,34 @@ sudo mount -o uid=pi,gid=pi /dev/sda1 /mnt/usbstorage
 # UUID=8765-4321 /mnt/usbstorage exfat nofail,uid=pi,gid=pi 0 0
 
 
+YOUTUBE
+TUTO : https://developers.google.com/youtube/v3/quickstart/python
+https://console.developers.google.com/apis/credentials?project=numeric-nova-183515
+
+sudo pip3 install --upgrade google-api-python-client
+sudo pip3 install --upgrade google-auth google-auth-oauthlib google-auth-httplib2
+
+
 
  ```
 
 
 https://www.raspberrypi.org/magpi/dropbox-raspberry-pi/
+
+
+
+
+
+## Dropbox API
+
+https://www.dropbox.com/developers-v1/core/start/python
+http://dropbox-sdk-python.readthedocs.io/en/latest/
+
+
+```
+https://www.dropbox.com/developers/apps/info/cl3b8z52tee5atf
+App key
+cl3b8z52tee5atf
+App secret
+3501nuov1s9p3p7
+```
--- watcher.py
+++ watcher.py
@@ -1,35 +1,95 @@
 
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+from progress.spinner import Spinner
 import time
+import glob
+import os.path
+import paramiko
+import sys
+from pprint import pprint
 
-
-class MyHandler(FileSystemEventHandler):
-    def on_modified(self, event):
-        print("on_modified")
 
 class CamCopyBot():
 	def __init__(self):
 		version = "0.0.1"
-		self.path  = "/mnt/usbstorage/DCIM/100MEDIA"
+		self.path  = "/mnt/usbstorage/DCIM/100MEDIA/"
 		print("CamCopyBot : "+version)
 
-		self.event_handler = MyHandler()
-		self.observer = Observer()
+		self.host = "192.168.0.39"                    #hard-coded
+		self.port = 22
+		self.transport = paramiko.Transport((host, port))
+		self.password = "julienterraz"                #hard-coded
+		self.username = "Ticita71$01"                #hard-coded
+		self.transport.connect(username = username, password = password)
 
-	
-
-		# Listen to new files added to the folder			
-		self.observer.schedule(self.event_handler, self.path , recursive=False)
-		self.observer.start()
+				
 
 
-		try:
-			while True:
-				time.sleep(1)
-				print("sleep..")
-		except KeyboardInterrupt:
-			self.observer.stop()
-		self.observer.join()
+
+		# self.dbx = dropbox.Dropbox('X-ZcIun6I8QAAAAAAAUhbEdNuWv-pVIko6L2wSsx6fd_yDRSmThQ-xpofCPVBVdT')
+		# self.dbx.users_get_current_account()
+		# print("?")
+		# pprint(self.dbx)
+		# for entry in self.dbx.files_list_folder('Applications').entries:
+  #  			print("entry:"+entry.name)
+						# finename = os.path.basename(vid)
+				# spinner = Spinner('Desrushing '+finename)
+				# spinner.next()
+
+		self.connectDropBox()
+		self.checkForVideoInPath()
+
+	def connectDropBox(_self):
+		print("Conencting to DropBox")
+		_self.dbx = dropbox.Dropbox('X-ZcIun6I8QAAAAAAAUhbEdNuWv-pVIko6L2wSsx6fd_yDRSmThQ-xpofCPVBVdT')
+		print ("linked account: ", _self.dbx)
+		pprint (_self.dbx)
+		for entry in _self.dbx.files_list_folder('/Applications/CamCopyBot').entries:
+  			print("entry:"+entry.name)
+
+	def copyVideos(_self,source):
+
+		dest = os.path.basename(source)
+		print("dest:"+dest)
+
+		#dbx.files_upload("Potential headline: Game 5 a nail-biter as Warriors inch out Cavs", '/cavs vs warriors/game 5/story.txt')
+
+		spinner = Spinner("copying file"+source)
+		# while True:
+		# 	spinner.next()
+			# _self.dbx.files_upload(source,'/Applications/CamCopyBot'+source)
+		with open(source, "rb") as f:
+			spinner.next()
+			_self.dbx.files_upload(f.read(), '/Applications/CamCopyBot'+dest, mute = False)
+
+
+
+	def checkForVideoInPath(self):	
+		spinner = Spinner('Waiting camera... ')
+		while True:
+			self.MP4 = glob.glob(self.path+"*.MP4")
+			spinner.next()
+			time.sleep(1)
+
+			if(len(self.MP4) > 0):
+				
+				for entry in self.MP4:
+					spinner = Spinner('found file'+entry)
+					spinner.next()
+					self.copyVideos(entry)
+				# break
+
+				
+
+			
+
+
+
 
 watcher = CamCopyBot()
+# watcher.checkForVideoInPath()
+
+
+
+
